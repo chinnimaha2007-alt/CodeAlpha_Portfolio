daaa50495b231ff520ffd9bb22cc1e8415f0d253
Mahalakshmi Vallem <chinnimaha.2007@gmail.com>
2026-06-10 22:27:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const text = [
+"Frontend Developer",
+"Cloud Computing Learner",
+"Future Software Engineer"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+
+count++;
+
+index = 0;
+
+setTimeout(type,1000);
+
+}else{
+
+setTimeout(type,100);
+
+}
+
+})();
+
+document
+.getElementById("themeBtn")
+.addEventListener("click",()=>{
+
+document.body.classList.toggle("dark");
+
+});
